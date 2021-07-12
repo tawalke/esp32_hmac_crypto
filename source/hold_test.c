0229112223e18cfa1c9ec05d619d0d954efa7d0d
Tara W <tawalke@users.noreply.github.com>
2021-07-12 04:07:17 -0400

    Create hold_test.c

---
+++ source/hold_test.c
@@ -0,0 +1,15 @@
+#include "esp_crypto.h"
+
+uint32_t Crypto_Init()
+{
+   return NULL;
+}
+
+uint32_t Crypto_HMAC( const uint8_t * pucKey, uint32_t ulKeyLength,
+                      const uint8_t * pucData, uint32_t ulDataLength,
+                      uint8_t * pucOutput, uint32_t ulOutputLength,
+                      uint32_t * pulBytesCopied )
+{
+	return NULL;
+  
+}
